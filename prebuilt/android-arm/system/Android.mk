b3e15d851fe8cd3c8c4f5d542676a3418d864a1a
fanjunwei <fanjunwei@baoxuetech.com>
2012-12-13 17:22:29 +0800

    iphone launcher initial layout

--- prebuilt/android-arm/system/Android.mk
+++ prebuilt/android-arm/system/Android.mk
@@ -7,8 +7,16 @@ $(patsubst ./%,%, \
  )
 endef
 
+define all-xml-files-under
+$(patsubst ./%,%, \
+  $(shell cd $(LOCAL_PATH) ; \
+          find $(1) -name "*.apk" -and -not -name ".*") \
+ )
+endef
+
 include $(CLEAR_VARS)
 copy_from := $(call all-apk-files-under)
+copy_from += $(call all-xml-files-under)
 copy_to := $(addprefix :system/,$(copy_from))
 copy_from := $(addprefix $(LOCAL_PATH)/,$(copy_from))
 copy_from := $(join $(copy_from),$(copy_to))

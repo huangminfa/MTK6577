7394443eed89ab4a5eb49cb409b21126781d6cdb
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2013-05-26 17:58:00 +0200

    some perms changed after script exec

--- mediatek/platform/mt6577/preloader/src/security/inc/KEY_SML_ENCODE.h
+++ mediatek/platform/mt6577/preloader/src/security/inc/KEY_SML_ENCODE.h
@@ -1,4 +1,4 @@
-// [/alps/mprelease/MPload/ALPS.ICS2.MP.V1.26_SUNMONTECH17_ICS2/source/alps/mediatek/platform/mt6577/preloader/src/security/inc/KEY_SML_ENCODE.h]
+// [/repos/MTK6577/mediatek/platform/mt6577/preloader/src/security/inc/KEY_SML_ENCODE.h]
 // BUILD TIME : Wed Mar 7 20:58:19 CST 2012
 #define SML_CUSTOM_AES_256 "A312312BC1986CB5465DD89713235BDC"
 #define CIPHER_OFFSET "0"
